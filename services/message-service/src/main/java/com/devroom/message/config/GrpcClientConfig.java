f6df057388c41f3bf9fd8eb73acb9566bc96b314
Annika Holmqvist <annika.holmqvist94@gmail.com>
2026-07-15 11:34:54 +0200

    feat(plan-15): gRPC observation interceptors for trace propagation

--- services/message-service/src/main/java/com/devroom/message/config/GrpcClientConfig.java
+++ services/message-service/src/main/java/com/devroom/message/config/GrpcClientConfig.java
@@ -2,6 +2,9 @@ package com.devroom.message.config;
 
 import com.devroom.user.grpc.UserGrpcServiceGrpc;
 import com.devroom.user.grpc.UserGrpcServiceGrpc.UserGrpcServiceBlockingStub;
+import io.grpc.ClientInterceptors;
+import io.micrometer.core.instrument.binder.grpc.ObservationGrpcClientInterceptor;
+import io.micrometer.observation.ObservationRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.grpc.client.GrpcChannelFactory;
@@ -10,7 +13,11 @@ import org.springframework.grpc.client.GrpcChannelFactory;
 public class GrpcClientConfig {
 
     @Bean
-    UserGrpcServiceBlockingStub userServiceStub(GrpcChannelFactory channels) {
-        return UserGrpcServiceGrpc.newBlockingStub(channels.createChannel("user-service"));
+    UserGrpcServiceBlockingStub userServiceStub(GrpcChannelFactory channels,
+                                                ObservationRegistry observationRegistry) {
+        var channel = channels.createChannel("user-service");
+        var traced = ClientInterceptors.intercept(channel,
+                new ObservationGrpcClientInterceptor(observationRegistry));
+        return UserGrpcServiceGrpc.newBlockingStub(traced);
     }
 }
